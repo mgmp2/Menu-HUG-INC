2a8fc73bb3acd16233b66b3a8267de5dab696bac
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-07 11:10:56 -0500

    appear logo and menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@
 //   e.preventDefault();
 //   document.getElementById("box-overlay").classList.add("active");
 // })
+
+document.getElementById("toggle").addEventListener("click", function(e){
+  e.preventDefault();
+document.getElementById("nav-header").classList.toggle("open");
+  document.getElementById('body').classList.toggle("overflow-hidden");
+})
