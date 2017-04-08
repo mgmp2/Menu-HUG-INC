8959f3ff95a97cbbf9621c8c77ef3c3506cb37f7
Miriam Mendoza Pilco <miriammp1997@gmail.com>
2017-04-08 02:13:27 -0500

    fix header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
-// document.getElementById("hamburger").addEventListener("click",
-// function(e){
-//   e.preventDefault();
-//   document.getElementById("box-overlay").classList.add("active");
-// })
+var listaCoder = document.getElementById("coders");
+var coders = ["Fiorella Quispe","Lulú Vilchez","Leidy Maldonado","Yessenia Huaman","Miriam Mendoza", "Elizabeth Condori",
+"Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana :v", "Jenny Velasquez", "Nadia Cuadros",
+"Mishel More", "Marilu Llamoca","Mariel García","Fiorella Cisneros", "Geraldine Chauca","Yelitza Choque","Stephanieh Iyagon",
+"Emma Tapia","Dana Franco","Flor Retamozo", "Nathal Pacheco","Erika Vidal","Katherine Ortega","Brilly Majuan","Flor Cantuta",
+"Leslie Aavendaño","Cindy Mendoza", "Annia Flores","Betsi Loayza","Ayda Sulca","Milagros Gutierrez","Nakarid Jave", "Angie Condor",
+"Maia Rojas","Ariana Cabana","Flor Condori", "Mitchell Condori", "Naomi Villanueva","Mary Castillo","Myriam Peralta","Karin Alejo",
+"Liliana Peña","Ruth Salvador", "Maribel :v","Wendy Reyes","Cynthia Quispe","MariaGrecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez",
+"Sandra Solorzano"];
 
-var listaCoder = document.getElementById("lista");
+
 document.getElementById("toggle").addEventListener("click", function(e){
   e.preventDefault();
       document.getElementById("nav-header").classList.toggle("open");
-      document.getElementById("nav-header").classList.toggle("active");
 
   document.getElementById('body').classList.toggle("overflow-hidden");
 })
@@ -41,42 +44,31 @@ window.addEventListener("scroll", function(){
 
 function createLiPictures(){
 
-  // <li class="box">
-  //   <div class="grid">
+  //   <div class="grid"> box
   //     <figure>
+  //       <div CLASS ="SOMBRA">
   //       <img src="assets/img/students/5.png" alt="Fiorella Quispe">
   //       <figcaption class="center">
-  //         <span>Fiorella Quispe</span>
+  //         Fiorella Quispe
   //       </figcaption>
   //     </figure>
   //   </div>
-  // </li>
-  var coders = ["Fiorella Quispe","Lulú Vilchez","Leidy Maldonado","Yessenia Huaman","Miriam Mendoza", "Elizabeth Condori",
-                "Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana :v", "Jenny Velasquez", "Nadia Cuadros",
-                "Mishel More", "Marilu Llamoca","Mariel García","Fiorella Cisneros", "Geraldine Chauca","Yelitza Choque","Stephanieh Iyagon",
-                "Emma Tapia","Dana Franco","Flor Retamozo", "Nathal Pacheco","Erika Vidal","Katherine Ortega","Brilly Majuan","Flor Cantuta",
-                "Leslie Aavendaño","Cindy Mendoza", "Annia Flores","Betsi Loayza","Ayda Sulca","Milagros Gutierrez","Nakarid Jave", "Angie Condor",
-                "Maia Rojas","Ariana Cabana","Flor Condori", "Mitchell Condori", "Naomi Villanueva","Mary Castillo","Myriam Peralta","Karin Alejo",
-                "Liliana Peña","Ruth Salvador", "Maribel :v","Wendy Reyes","Cynthia Quispe","MariaGrecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez",
-                "Sandra Solorzano"];
   for (var i = 1; i < 55; i++) {
-    li = document.createElement("li");
-    li.setAttribute("class", "box");
-    fig = document.createElement("figure");
-    div = document.createElement("div");
-    div.setAttribute("class","sombra");
-    img= document.createElement("img");
+    var grid = document.createElement("div");
+    grid.setAttribute("class","grid");
+    var fig = document.createElement("figure");
+    /* start children*/
+    var img= document.createElement("img");
     img.setAttribute("src","assets/img/students/"+i+".png");
     img.setAttribute("alt",coders[i-1]);
-    figcap= document.createElement("figcaption");
+    var figcap= document.createElement("figcaption");
     figcap.setAttribute("class","center");
-    span = document.createElement("span");
-    span.innerText = coders[i-1];
+    var span = document.createTextNode(coders[i-1]);
+    /*finish childredn */
     figcap.appendChild(span);
-    fig.appendChild(div);
-    fig.appendChild(img);
     fig.appendChild(figcap);
-    li.appendChild(fig);
-    listaCoder.appendChild(li);
+    fig.appendChild(img);
+    grid.appendChild(fig);
+    listaCoder.appendChild(grid);
   };
 }
